a5cbe21772e332d05aa86add32709003294c64d2
andyV-323 <40248914+andyV-323@users.noreply.github.com>
2022-10-23 16:10:41 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -129,6 +129,13 @@
 				hamburger.classList.remove("active");
 			navMenu.classList.remove("active");
 			}))
+		function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 			
 
 		
