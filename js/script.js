69a3b9413de001fc2c1e6fc02ccf7615a590141e
andyV-323 <40248914+andyV-323@users.noreply.github.com>
2023-04-03 17:48:49 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,98 +1,96 @@
-		
+function myFunction() {
+		var dots = document.getElementById("dots");
+		var moreText = document.getElementById("more");
+		var btnText = document.getElementById("myBtn");
+		var image = document.getElementById("memoji2");
+
+		if (dots.style.display === "none") {
+		dots.style.display     = "inline";
+		btnText.innerHTML      = "Read more";
+		moreText.style.display = "none";
+		image.style.marginTop  = "250px";
+    } else {
+		dots.style.display     = "none";
+		btnText.innerHTML      = "Read less";
+		moreText.style.display = "inline";
+		image.style.marginTop  = "450px";
+    }
+}
 //how fast the logo stays on then fades out
-		$(window).on("load", function(){
-			$(".loader .inner").fadeOut(1000, function() {
-				$(".loader").fadeOut(1100);
-			});
-		})
-	
-
-
-
-//how fast the slides fade to next
-	$ (document).ready(function(){
-
-		$('#slides').superslides({
-			animation:'fade', 
-			play: 3100,
-			pagination: 0
-
-		});
+$(window).on("load", function() {
+    $(".loader .inner").fadeOut(1000, function() {
+        $(".loader").fadeOut(1100);
+    });
+})
 
 
 //the animation for the letters being typed on slides
-		var typed = new Typed(".typed", {
-			strings: ["Software Engineer.", "Student."],
-			typeSpeed: 110,
-			loop: true,
-			startDelay: 1000,
-			showCursor: false
+$(document).ready(function() {
+    var typed = new Typed(".typed", {
+        strings: ["&lt; &#8725; Software Engineer&gt;", "&lt; &#8725; Mobile App Developer&gt;",
+            "&lt; &#8725; Full Stack Developer&gt;", "&lt; &#8725; Student&gt;"
+        ],
+        typeSpeed: 70,
+        backspeed: 200,
+        smartBackspace: true,
+        loop: true,
+        showCursor: true
 
-		});
-
-		
-		//the isotope animation for the thumbnails
-		
-		$(".items").isotope({
-			filter: '*',
-			animationsOptions: {
-				duration: 1500,
-				easing:'linear',
-				queue: false
-			}
-		});
+    });
 
 
-//filter buttons 
-		$("#filters a").click(function() {
-			$("#filters .current").removeClass("current");
-			$(this).addClass("current");
+    //the isotope animation for the thumbnails
 
-			var selector = $(this).attr("data-filter");
+    $(".items").isotope({
+        filter: '*',
+        animationsOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
 
-			$(".items").isotope({
-			filter: selector,
-			animationOptions: {
-				duration: 1500,
-				easing:'linear',
-				queue: false
-			}
 
-		});
+    //filter buttons 
+    $("#filters a").click(function() {
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
 
-			return false;
+        var selector = $(this).attr("data-filter");
 
-		});
-		});
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
 
-		$(".nav-item  a").click(function(e) {
-			e.preventDefault();
+        });
 
-			var targetElement = $(this).attr("href");
-			var targetPosition = $(targetElement).offset().top;
-			$("html, body").animate({scrollTop: targetPosition - 100},"slow");
-		})
+        return false;
+
+    });
+});
+
+$(".nav-item  a").click(function(e) {
+    e.preventDefault();
+
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;
+    $("html, body").animate({ scrollTop: targetPosition - 100 }, "slow");
+})
 //hamburger on small navigation bar menu open and close
-		const hamburger = document.querySelector(".hamburger");
-		const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-		hamburger.addEventListener("click" , () => {
-			hamburger.classList.toggle("active");
-			navMenu.classList.toggle("active");
-		})
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-		document.querySelectorAll(".nav-link").forEach(n => n
-			.addEventListener("click", () => {
-				hamburger.classList.remove("active");
-			navMenu.classList.remove("active");
-			}))
-
-
-
-
-
-
-		
-	
-
-	
+document.querySelectorAll(".nav-link").forEach(n => n
+    .addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
